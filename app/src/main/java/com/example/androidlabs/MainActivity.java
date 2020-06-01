5d58f56c48170b07cca93c92633ce1d8436ef7d3
Aimen <hall0408@algonquinlive.com>
2020-06-01 16:24:46 -0400

    activity_main_linear

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
+        //Image button
+        ImageButton img = findViewById(R.id.imgBtn);
+//        Switch swch = findViewById(R.id.swch);
+//        swch.setOnClickListener( v -> {
+//            Context context = getApplicationContext();
+//            CharSequence text = "Hello toast!";
+//            int duration = Toast.LENGTH_LONG;
+//
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+//        });
     }
 }
