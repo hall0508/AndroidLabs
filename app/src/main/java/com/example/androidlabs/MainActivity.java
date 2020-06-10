99f5f32fb2278cbb0ac00fd4182680802e0e338a
Aimen <hall0408@algonquinlive.com>
2020-06-10 07:57:06 -0400

    part 1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,55 +3,44 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public EditText tv;
+    public Button btn;
+    public boolean temp;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Layouts
-        //setContentView(R.layout.activity_main);
-        //setContentView(R.layout.activity_main_linear);
-        //setContentView(R.layout.activity_main_grid);
-        setContentView(R.layout.activity_main_relative);
-        //Tag every object
-        LinearLayout layout = findViewById(R.id.layout);
-        ImageButton img = findViewById(R.id.imgBtn);
-        Switch swch = findViewById(R.id.swch);
-        CheckBox check = findViewById(R.id.check_this_out);
-        Button btn = findViewById(R.id.clickhere);
-        //Listeners
-        check.setOnClickListener( v -> {
-            Toast toast = Toast.makeText(getApplicationContext(),getResources().getString(R.string.toast_message), Toast.LENGTH_LONG);
-            toast.show();
-            //Snackbar snackbar = Snackbar.make(layout,"The switch is now On",Snackbar.LENGTH_SHORT);
-            //snackbar.show();
-        });
-        check.setOnCheckedChangeListener(((buttonView, isChecked) -> {
-            if (isChecked) {
-                Snackbar.make(check, R.string.checkOn, Snackbar.LENGTH_LONG).setAction(R.string.undo, v -> check.setChecked(false)).show();
-            }else {
-                Snackbar.make(check, R.string.checkOff, Snackbar.LENGTH_LONG).setAction(R.string.undo, v -> check.setChecked(true)).show();
-            }
-        }));
-        swch.setOnCheckedChangeListener(((buttonView, isChecked) -> {
-            if (isChecked) {
-                Snackbar.make(swch, R.string.swchOn, Snackbar.LENGTH_LONG).setAction(R.string.undo, v -> swch.setChecked(false)).show();
-            }else {
-                Snackbar.make(swch, R.string.swchOff, Snackbar.LENGTH_LONG).setAction(R.string.undo, v -> swch.setChecked(true)).show();
-            }
-
-        }));
+        setContentView(R.layout.activity_main);
+         tv = findViewById(R.id.editTextTextEmailAddress);
+         btn = findViewById(R.id.button);
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        tv.setText(sp.getString("Email",""));
+            btn.setOnClickListener( l -> {
+                temp = true;
+            });
         }
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (temp){
+            SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("Email",tv.getText().toString());
+            editor.commit();
+        }
+    }
 
     }
---
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -0,0 +1,41 @@
+    package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageButton;
+
+    public class ProfileActivity extends AppCompatActivity {
+    public ImageButton mImageButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mImageButton = findViewById(R.id.img);
+        mImageButton.setOnClickListener(l -> {
+    dispatchTakePictureIntent();
+        });
+    }
+        private void dispatchTakePictureIntent() {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                mImageButton.setImageBitmap(imageBitmap);
+            }
+        }
+
+
+    }
